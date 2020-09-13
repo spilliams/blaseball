b65e85f566c305e092c83426d4aa44da155f375e
Spencer Williams <s@spencerenglish.com>
2020-09-13 09:03:17 -0700

    cli should use logrus as verbose logger

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
 
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -14,10 +15,25 @@ type contextLabel string
 
 const (
 	apiLabel contextLabel = "api"
+
+	customAPIFlag = "custom-api"
+	localAPIFlag  = "local-api"
+	remoteAPIFlag = "remote-api"
+	verboseFlag   = "verbose"
+)
+
+var (
+	customAPIURL       string
+	forbiddenKnowledge bool
+	localAPI           bool
+	remoteAPI          bool
+	verbose            bool
 )
 
 func main() {
 	rootCmd := newRootCmd()
+
+	cobra.OnInitialize(initLogger)
 
 	rootCmd.AddCommand(newDivisionsCmd())
 
@@ -27,95 +43,51 @@ func main() {
 	}
 }
 
-const (
-	verboseFlag   = "verbose"
-	customAPIFlag = "custom-api"
-	remoteAPIFlag = "remote-api"
-	localAPIFlag  = "local-api"
-)
-
 // newRootCmd returns the blasball base command.
 func newRootCmd() *cobra.Command {
-	var flags struct {
-		forbiddenKnowledge bool
-		verbose            bool
-		customAPI          string
-		localAPI           bool
-		remoteAPI          bool
-	}
 	cmd := &cobra.Command{
 		Use:   "blase",
 		Short: "A tool for getting details about blaseball",
 	}
 
-	cmd.PersistentFlags().BoolVarP(&flags.forbiddenKnowledge, "forbidden-knowledge", "f", false, "Display forbidden knowledge")
-	cmd.PersistentFlags().BoolVarP(&flags.verbose, verboseFlag, "v", false, "Display verbose output")
-	cmd.PersistentFlags().StringVar(&flags.customAPI, customAPIFlag, "", "Use a custom API")
-	cmd.PersistentFlags().BoolVarP(&flags.localAPI, localAPIFlag, "l", false, "Use the API at http://localhost:8080/")
-	cmd.PersistentFlags().BoolVarP(&flags.remoteAPI, remoteAPIFlag, "r", true, "Use the API at https://www.blaseball.com/database/")
+	cmd.PersistentFlags().StringVar(&customAPIURL, customAPIFlag, "", "Use a custom API")
+	cmd.PersistentFlags().BoolVarP(&forbiddenKnowledge, "forbidden-knowledge", "f", false, "Display forbidden knowledge")
+	cmd.PersistentFlags().BoolVarP(&localAPI, localAPIFlag, "l", false, "Use the API at http://localhost:8080/")
+	cmd.PersistentFlags().BoolVarP(&remoteAPI, remoteAPIFlag, "r", true, "Use the API at https://www.blaseball.com/database/")
+	cmd.PersistentFlags().BoolVarP(&verbose, verboseFlag, "v", false, "Display verbose output")
 
 	return cmd
 }
 
+func initLogger() {
+	if verbose {
+		logrus.SetLevel(logrus.DebugLevel)
+	} else {
+		logrus.SetLevel(logrus.InfoLevel)
+	}
+}
+
 func resolveAPI(cmd *cobra.Command) (pkg.RemoteDataSession, error) {
-	customAPIURL, err := cmd.Flags().GetString(customAPIFlag)
-	if err != nil {
-		return nil, err
-	}
-	localAPI, err := cmd.Flags().GetBool(localAPIFlag)
-	if err != nil {
-		return nil, err
-	}
-	remoteAPI, err := cmd.Flags().GetBool(remoteAPIFlag)
-	if err != nil {
-		return nil, err
-	}
 	// resolve API url
 	apiURL := ""
 	if len(customAPIURL) != 0 {
-		vlog(cmd, "using custom api url %s", customAPIURL)
+		logrus.Debugf("using custom api url %s", customAPIURL)
 		apiURL = customAPIURL
 	} else if localAPI {
-		vlog(cmd, "using local api url (http://localhost:8080/)")
+		logrus.Debug("using local api url (http://localhost:8080/)")
 		apiURL = "http://localhost:8080/"
 	} else if remoteAPI {
-		vlog(cmd, "using remote api url (https://www.blaseball.com/database/)")
+		logrus.Debug("using remote api url (https://www.blaseball.com/database/)")
 		apiURL = "https://www.blaseball.com/database/"
 	} else {
 		return nil, fmt.Errorf("no API URL specified. Please use one of --%s, --%s or --%s", customAPIFlag, localAPIFlag, remoteAPIFlag)
 	}
 
-	verbose, err := cmd.Flags().GetBool(verboseFlag)
-	if err != nil {
-		return nil, err
-	}
-	level := logrus.InfoLevel
-	if verbose {
-		level = logrus.DebugLevel
-	}
-
-	apiService := api.NewAPI(apiURL, level)
+	apiService := api.NewAPI(apiURL, logrus.GetLevel())
 	return apiService, nil
 }
 
-// TODO: use logrus levels for the CLI too
-func vlog(cmd *cobra.Command, format string, parts ...interface{}) {
-	verbose, err := cmd.Flags().GetBool(verboseFlag)
-	if err != nil {
-		panic(err)
-	}
-	if !verbose {
-		return
-	}
-
-	msg := fmt.Sprintf(format, parts...)
-	if msg[len(msg)-1] != '\n' {
-		msg += "\n"
-	}
-	fmt.Print(msg)
-}
-
 func isGUID(s string) bool {
-	// TODO
-	return false
+	guidRE := regexp.MustCompile("[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}")
+	return guidRE.MatchString(s)
 }
