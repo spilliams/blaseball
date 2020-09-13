bfd1ceb5dcd93dfcb8d308b34d7fa7a54d6aecb6
Spencer Williams <s@spencerenglish.com>
2020-09-13 09:21:13 -0700

    local data model should match remote data model

--- pkg/model/division.go
+++ pkg/model/division.go
@@ -1,12 +1,15 @@
 package model
 
+import "fmt"
+
 // Division represents a division of teams. In season play, teams usually play
 // other teams from their division.
 type Division struct {
-	ID   string
-	Name string
+	ID      string
+	Name    string
+	TeamIDs []string `json:"teams"`
 }
 
 func (d *Division) String() string {
-	return d.Name
+	return fmt.Sprintf("<D %s (%d teams)>", d.Name, len(d.TeamIDs))
 }
